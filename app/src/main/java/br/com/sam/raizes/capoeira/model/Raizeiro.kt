364c8507e5a391e82ec642695316b668df00ab37
Samantha <samgomescosta@gmail.com>
2020-12-09 16:02:28 -0300

    first commit already with class models

---
+++ app/src/main/java/br/com/sam/raizes/capoeira/model/Raizeiro.kt
@@ -0,0 +1,13 @@
+package br.com.sam.raizes.capoeira.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Raizeiro(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val nickname: String = "",
+    val name: String = "",
+    val graduation: Graduation
+)
---
+++ app/src/main/java/br/com/sam/raizes/capoeira/ui/RaizeiroActivity.kt
@@ -0,0 +1,13 @@
+package br.com.sam.raizes.capoeira.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.sam.raizes.capoeira.R
+
+class RaizeiroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
